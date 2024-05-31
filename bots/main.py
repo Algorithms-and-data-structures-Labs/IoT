dab5c3c79fd3e74b5843c4267d5dd62311b59cef
Semyon1104 <sema_titov_04@mail.ru>
2024-05-31 18:57:36 +0300

    fix bots

--- bots/main.py
+++ bots/main.py
@@ -5,6 +5,15 @@ from random import randint, uniform
 from requests import post as rq_post
 from datetime import datetime, timedelta
 from configparser import ConfigParser
+import time
+
+start_time = time.time()
+def get_duration(start_time):
+    current_time = time.time()
+    elapsed_time = current_time - start_time
+    duration = 1 * (elapsed_time // INTERVAL)
+    duration = duration % 10
+    return duration
 
 config = ConfigParser()
 path = os.path.dirname(os.path.abspath(argv[0]))
@@ -19,7 +28,7 @@ TOKENS = {
 
 MIN_INTERVAL = int(config['TIMER']['MIN_INTERVAL'])
 MAX_INTERVAL = int(config['TIMER']['MAX_INTERVAL'])
-INTERVAL = randint(MIN_INTERVAL, MAX_INTERVAL)
+INTERVAL = 10
 
 MIN_START_TIME = int(config['TELEMETRY.TIMER']['MIN_START_TIME'])
 MAX_START_TIME = int(config['TELEMETRY.TIMER']['MAX_START_TIME'])
@@ -39,12 +48,13 @@ URL = config["WEB"]["URL"]
 
 TIMER = -1
 
+
 def post(token, value):
     if (token == "-"):
         return
-    
+
     try:
-        respose = rq_post(URL, params={'token' : token}, json=value)
+        respose = rq_post(URL, params={'token': token}, json=value)
         print(f"POST /?token={token} {value.items()} responce code: {respose.status_code}")
     except Exception as e:
         print(e)
@@ -53,7 +63,9 @@ def post(token, value):
         TIMER.cancel()
         exit()
 
+
 def post_values():
+    duration = 0
     day = 1
     hour_falling = randint(MIN_START_TIME, MAX_START_TIME)
     if (hour_falling > 23):
@@ -61,8 +73,7 @@ def post_values():
         hour_falling %= 24
 
     time_falling = (datetime.now() - timedelta(days=day)).replace(hour=hour_falling)
-
-    duration = randint(MIN_DURATION, MAX_DURATION)
+    duration = get_duration(start_time)
 
     post(TOKENS['timer'], {
         'start_time': time_falling.isoformat(),
@@ -78,14 +89,15 @@ def post_values():
     pressure = randint(MIN_PRESSURE_VALUE, MAX_PRESSURE_VALUE)
 
     post(TOKENS['pressureSensor'], {
-        'pressureSensor': pressure
+        'pressure': pressure
     })
 
     level = randint(MIN_LEVEL, MAX_LEVEL)
 
     post(TOKENS['levelSensor'], {
-        'levelSensor': level
+        'level': level
     })
+
 
 def repeater(interval, function):
     global TIMER
@@ -93,8 +105,10 @@ def repeater(interval, function):
     TIMER.start()
     function()
 
+
 def main():
     repeater(INTERVAL, post_values)
 
-if __name__  == '__main__':
+
+if __name__ == '__main__':
     main()
